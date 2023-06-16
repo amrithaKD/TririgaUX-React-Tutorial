57d87da12b1cfa8f6bc2e9a0e47d4ebb6373d824
Amritha Das <Amritha.Das@ibm.com>
2023-06-16 15:43:33 +0530

    initial commit
    initial commit

---
+++ my-first-app/src/pages/index.js
@@ -0,0 +1,5 @@
+import HomePage from "./HomePage/HomePage";
+import CurrentUserPage from "./CurrentUserPage/CurrentUserPage";
+import UnauthorizedPage from "./UnauthorizedPage/UnauthorizedPage";
+import AllBuildingsPage from "./AllBuildingsPage/AllBuildingsPage";
+export { HomePage, CurrentUserPage, UnauthorizedPage, AllBuildingsPage };
